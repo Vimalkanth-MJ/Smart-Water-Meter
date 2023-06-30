1be6e9a966c4de2d1f22b57658a738eb0e5d82ba
Vimalkanth-MJ <vimalkanth2002@gmail.com>
2023-06-30 19:13:00 +0530

    Flow Rate Implemented Successfully

--- Slave_Code/Slave_Code.ino
+++ Slave_Code/Slave_Code.ino
@@ -1,18 +1,32 @@
 int sensorPin = 4;
-volatile long pulse;
+volatile long pulse,pulse1;
 float volume;
+unsigned long oldTime;
+float flowRate;
+const float FLOW_CALIBRATION = 7.5; // Calibration factor for flow sensor
 
 void setup() {
   pinMode(sensorPin, INPUT);
   Serial.begin(115200);
   attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
+  oldTime = millis(); // Initialize oldTime variable
 }
 
 void loop() {
-  volume = 2.120*pulse/1000; 
-  Serial.printf("Total volume:%f L\n",volume);
+  if ((millis() - oldTime) > 1000) { // Only calculate flow rate once per second
+    detachInterrupt(digitalPinToInterrupt(sensorPin));
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulse)/FLOW_CALIBRATION;
+    volume =  2.126 * pulse1 / 1000; 
+    Serial.printf("Flow rate: %.2f L/min\n", flowRate);
+    Serial.printf("Total volume: %.3f L\n", volume);
+    pulse = 0; // Reset pulse count
+    oldTime = millis(); // Update oldTime
+    attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
+  }
   delay(500);
 }
+
 void increase() {
   pulse++;
+  pulse1++;
 }
