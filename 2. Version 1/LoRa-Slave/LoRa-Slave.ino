247751052a6c3dbb8b2d29deeb386caec38c49bc
Vimalkanth-MJ <vimalkanth2002@gmail.com>
2023-07-03 13:31:07 +0530

    V1 Update

--- 2. Version 1/LoRa-Slave/LoRa-Slave.ino
+++ 2. Version 1/LoRa-Slave/LoRa-Slave.ino
@@ -1,26 +1,52 @@
 //----------------------------------------
 //        Defining Header Files
 //----------------------------------------
+
 #include <SPI.h>
 #include <LoRa.h>
 //Libraries for OLED Display
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+//----------------------------------------
+//     Enable/Disable the Debugging
+//----------------------------------------
+
+#define DEBUG // Enable debugging
+
+#ifdef DEBUG
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#endif
+
 //----------------------------------------
 //      Defining Display Variables
 //----------------------------------------
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 //----------------------------------------
-//      Defining LoRa Variables
+//      Defining PIN Config Variables
 //----------------------------------------
+
 #define ss 5
 #define rst 14
 #define dio0 2
+int sensorPin = 4;
+int relayPin = 17;
+
+//----------------------------------------
+//      Defining LoRa Variables
+//----------------------------------------
+
 String Incoming = "";
 String Message = "";
 String DeviceID = "Slave01";
@@ -28,17 +54,21 @@ byte LocalAddress = 0x02;       //--> address of this device (Slave 1).
 bool executed = false;
 bool msgRecieved = false;
 byte Destination_Master = 0x01; //--> destination to send to Master (ESP32).
+
 //----------------------------------------
-//      Defining Variables
+//  Defining Variables for Flow Sensor
 //----------------------------------------
-int sensorPin = 4;
-int relayPin = 26;
+
 volatile long pulse, pulse1;
 float cost, creditsOld, totalCredits, flowCredits;
 float flowRate, totalLitres, totalLitresOld, flowLitres;
 unsigned long oldTime;
 unsigned long previousTime = 0;
 const float FLOW_CALIBRATION = 7.5;
+
+//----------------------------------------
+//  Function for Sending LoRa Message
+//----------------------------------------
 
 void sendMessage(String Outgoing, byte Destination) {
   LoRa.beginPacket();             //--> start packet
@@ -49,8 +79,12 @@ void sendMessage(String Outgoing, byte Destination) {
   LoRa.endPacket();               //--> finish packet and send it
 }
 
+//----------------------------------------
+//  Function to reicieve LoRa Message
+//----------------------------------------
+
 void onReceive(int packetSize) {
-  if (packetSize == 0) return;  //--> if there's no packet, return
+  if (packetSize == 0) return;        //--> if there's no packet, return
 
   //read packet header bytes:
   int recipient = LoRa.read();        //--> recipient address
@@ -66,26 +100,28 @@ void onReceive(int packetSize) {
 
   //Check length for error.
   if (incomingLength != Incoming.length()) {
-    Serial.println();
-    Serial.println("error: message length does not match length");
+    DEBUG_PRINTLN();
+    DEBUG_PRINTLN("error: message length does not match length");
     return; //--> skip rest of function
   }
 
   //Checks whether the incoming data or message for this device.
   if (recipient != LocalAddress) {
-    Serial.println();
-    Serial.println("This message is not for me.");
+    DEBUG_PRINTLN();
+    DEBUG_PRINTLN("This message is not for me.");
     return; //--> skip rest of function
   }
 
-  Serial.println();
-  Serial.println("Received from: 0x" + String(sender, HEX));
-  Serial.println("Message: " + Incoming);
-  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  DEBUG_PRINTLN();
+  DEBUG_PRINTLN("Received from: 0x" + String(sender, HEX));
+  DEBUG_PRINTLN("Message: " + Incoming);
+  DEBUG_PRINTLN("RSSI: " + String(LoRa.packetRssi()));
   Processing_incoming_data();
   msgRecieved = true;
 }
-
+//----------------------------------------
+// Function to process the Incoming Data
+//----------------------------------------
 void Processing_incoming_data()
 {
   int pos1 = Incoming.indexOf('/');
@@ -97,84 +133,89 @@ void Processing_incoming_data()
     totalLitresOld = atof(Incoming.substring(pos1 + 1, pos2).c_str());
     creditsOld = atof(Incoming.substring(pos2 + 1, pos3).c_str());
     cost = atof(Incoming.substring(pos3 + 1, Incoming.length()).c_str());
-    Serial.println("Incoming LoRa Data=====================================================================");
-    Serial.printf("Cost per Litre: %.2f \n", cost);
-    Serial.printf("Available Credits: %.2f \n", creditsOld);
-    Serial.printf("Volume Consumed: %.2f \n", totalLitresOld);
-    Serial.println();
-    Serial.println("============================================================================");
+    DEBUG_PRINTLN("===============Incoming LoRa Data===============");
+    DEBUG_PRINT("Cost per Litre: ");
+    DEBUG_PRINTLN(cost);
+    DEBUG_PRINT("Available Credits: ");
+    DEBUG_PRINTLN(creditsOld);
+    DEBUG_PRINT("Volume Consumed: ");
+    DEBUG_PRINTLN(totalLitresOld);
+    DEBUG_PRINTLN();
+    DEBUG_PRINTLN("=================================================");
     totalCredits = creditsOld;
   }
 }
 //----------------------------------------
-//           void getReadings()
+//  getReadings() for Flow Calculations
 //----------------------------------------
 void getReadings() {
   if (totalCredits > 0)
   {
-    if ((millis() - oldTime) > 1000) { // Only calculate flow rate once per second
-      detachInterrupt(digitalPinToInterrupt(sensorPin));
-      flowRate = ((1000.0 / (millis() - oldTime)) * pulse) / FLOW_CALIBRATION;
-      oldTime = millis(); // Update oldTime
-      flowLitres =  2.126 * pulse1 / 1000;
-      totalLitres = totalLitresOld + flowLitres;
-      flowCredits = flowLitres * cost;
-      totalCredits = creditsOld - flowCredits;
-      digitalWrite(relayPin, HIGH);
-      Serial.printf("Flow rate: %.2f L/min\n", flowRate);
-      Serial.printf("Total volume: %.3f L\n", totalLitres);
-      Serial.printf("Avl. Credits: %.2f L\n", totalCredits);
-      Serial.println();
-      Serial.println();
-      Serial.println();
-      DisplayData();
+    if ((millis() - oldTime) > 1000) {                                         // Only calculate flow rate once per second
+      detachInterrupt(digitalPinToInterrupt(sensorPin));                       // Disable interrupt to prevent further pulse count
+      flowRate = ((1000.0 / (millis() - oldTime)) * pulse) / FLOW_CALIBRATION; // Calculate flow rate in liters per minute
+      oldTime = millis();                                                      // Update oldTime
+      flowLitres =  2.126 * pulse1 / 1000;                                     // Calculate the flow in liters since the last calculation
+      totalLitres = totalLitresOld + flowLitres;                               // Update the total volume consumed by adding the flow since the last calculation
+      flowCredits = flowLitres * cost;                                         // Calculate the credits consumed based on the flow and cost per liter
+      totalCredits = creditsOld - flowCredits;                                 // Update the available credits by subtracting the credits consumed
+      digitalWrite(relayPin, HIGH);                                            // TurnOn the Solenoid Valve For Water Flow
+      DisplayData();                                                           // Update the Data to Integrated Display
       if (flowRate > 0)
       {
-        sendLoRaData();
-        Serial.println("Sending Data over LoRa");
+        sendLoRaData();                                                        // if Flow is detected, sends the Data to Server Node Over LoRa Network
+        DEBUG_PRINTLN("Sending Data over LoRa");
       }
-      pulse = 0; // Reset pulse count
+      pulse = 0;                                                               // Reset the pulse count and reattach the interrupt
       attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
     }
   }
   else {
+     // No credits available, reset values and send LoRa data
     totalCredits = 0;
     totalLitres = 0;
     flowLitres = 0;
     flowCredits = 0;
     pulse = 0; pulse1 = 0;
     oldTime = millis();
+    // Send LoRa data only once when there are no available credits
     if (!executed) {
       sendLoRaData();
       executed = true;
     }
-    digitalWrite(relayPin, LOW);
-    DisplayNoData();
-    getDataFromLoRa();
-    onReceive(LoRa.parsePacket());
-    delay(500);
-    Serial.println("Waiting for packets");
+    digitalWrite(relayPin, LOW);    // Turn OFF the Solenoid Valve to prevent Water Flow
+    DisplayNoData();                // Display "No Data" message on the Integrate Display
+    getDataFromLoRa();              // Request updated data from the master device over LoRa
+    onReceive(LoRa.parsePacket());  // Check for any incoming LoRa packets
+    DEBUG_PRINTLN("Waiting for packets");
+    delay(1000);                    // Delay for stability and wait for incoming packets
   }
 }
 
-//----------------------------------------
-//           void sendLoRaData()
-//----------------------------------------
+//---------------------------------------------------------------------------------
+//           Function To send the Update Data to Master Node
+//---------------------------------------------------------------------------------
 void sendLoRaData() {
   Message = String(DeviceID) + "/" + String(totalLitres) + "&" + String(totalCredits) + "#" + String(cost);
   sendMessage(Message, Destination_Master);
 }
-
+//-----------------------------------------------------------------------------------
+//           Function To Update Pulse COunt Used In Interrupt
+//-----------------------------------------------------------------------------------
 void increase() {
   pulse++;
   pulse1++;
 }
-
+//------------------------------------------------------------------------------------
+//           Function To Request updated data from the master device over LoRa
+//------------------------------------------------------------------------------------
 void getDataFromLoRa()
 {
   sendMessage("Hi", Destination_Master);
 }
-
+//------------------------------------------------------------------------------------
+//           Function that Diplays the Data over Integrated Screen
+//------------------------------------------------------------------------------------
 void DisplayData()
 {
   display.clearDisplay();
@@ -198,6 +239,9 @@ void DisplayData()
   display.print(totalCredits);
   display.display();
 }
+//------------------------------------------------------------------------------------
+//           Function To Display "No Data" message on the Integrated Screen
+//------------------------------------------------------------------------------------
 void DisplayNoData()
 {
   display.clearDisplay();
@@ -212,52 +256,37 @@ void DisplayNoData()
   display.print("Please Recharge.!");
   display.display();
 }
-
+//------------------------------------------------------------------------------------
+//                              Setup() to Initialise
+//------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
   LoRa.setPins(ss, rst, dio0);
-  Serial.println("Start LoRa init...");
+  DEBUG_PRINTLN("Start LoRa init...");
   if (!LoRa.begin(433E6)) {
-    Serial.println("LoRa init failed. Check your connections.");
+    DEBUG_PRINTLN("LoRa init failed. Check your connections.");
     while (true);
   }
-  Serial.println("LoRa init succeeded.");
+  DEBUG_PRINTLN("LoRa init succeeded.");
   pinMode(sensorPin, INPUT);
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
   attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("Display init Failed"));
+    DEBUG_PRINTLN(F("Display init Failed"));
     for (;;);
   }
   display.display();
   delay(2000);
   display.clearDisplay();
-  oldTime = millis(); // Initialize oldTime variable
+  oldTime = millis();
   getDataFromLoRa();
 }
-
+//------------------------------------------------------------------------------------
+//                              Setup() to Iterate
+//------------------------------------------------------------------------------------
 void loop() {
   onReceive(LoRa.parsePacket());
-  if (msgRecieved == true)
-  {
-    Serial.println("Sleeping");
-    Serial.printf("Flow rate: %.2f L/min\n", flowRate);
-    Serial.printf("cost: %.2f L/min\n", cost);
-    Serial.printf("creditsOld: %.2f L/min\n", creditsOld);
-    Serial.printf("totalCredits: %.2f L/min\n", totalCredits);
-    Serial.printf("flowCredits: %.2f L/min\n", flowCredits);
-    Serial.printf("totalLitres: %.2f L/min\n", totalLitres);
-    Serial.printf("totalLitresOld: %.2f L/min\n", totalLitresOld);
-    Serial.printf("flowLitres: %.2f L/min\n", flowLitres);
-    Serial.println();
-    Serial.println();
-    Serial.println();
-    Serial.println();
-    Serial.println();
-    msgRecieved = false;
-    delay(5000);
-  }
   getReadings();
 }
---
+++ 2. Version 1/LoRa-Slave/README.md
@@ -0,0 +1,52 @@
+
+# Code WorkFlow
+
+1. Initialize the serial communication and LoRa module.
+2. Connect to the Blynk server using the specified authentication token and Wi-Fi credentials.
+3. Set up a timer to periodically send data to the Blynk app.
+4. Enter the `loop()` function.
+5. Check for incoming LoRa packets using `LoRa.parsePacket()`.
+6. If a packet is received, go to the `onReceive(int packetSize)` function.
+7. Read the packet header to get the recipient, sender, and message length.
+8. Read the payload data and store it in the `Incoming` variable.
+9. Check the length of the incoming message for data integrity.
+10. If the message is for the current device, process it accordingly.
+11. If the message starts with "Hi", prepare a response message based on the sender's address and send it using LoRa.
+12. If the message is a data update, parse the values for `volume1`, `credits1`, and `cost1`, and update the variables.
+13. Send the updated data to the Blynk app using the respective virtual pins.
+14. Exit the `onReceive()` function and continue with the `loop()` function.
+15. Run the Blynk library using `Blynk.run()` to maintain the Blynk connection.
+16. Run the Blynk timer using `timer.run()` to trigger the periodic data update to the Blynk app.
+17. Repeat the loop and continue listening for LoRa packets and updating the Blynk app.
+
+This flow ensures that the ESP32 receives LoRa packets, processes the received data, updates the Blynk app, and maintains the communication with both LoRa and Blynk services.
+
+## Functions Explained:
+
+Sure! Let's go through each function in the provided code and explain its purpose and how it works.
+
+📌 `BLYNK_CONNECTED()`: This is a Blynk library callback function that is called when the ESP32 connects to the Blynk server. In this code, it is used to synchronize the virtual pins V1, V3, V4, and V5 with their corresponding values on the Blynk server.
+
+📌 `BLYNK_WRITE(V1)`: This is another Blynk callback function that is called whenever the value of virtual pin V1 is updated on the Blynk app. In this code, it updates the `credits1` variable with the new value received from the app and then calls `sendDataToClient()` and `computeRemainingVolume()` functions.
+
+📌 `BLYNK_WRITE(V3)`: This function is similar to the previous one but it is triggered when the value of virtual pin V3 is updated on the Blynk app. It updates the `volume1` variable with the new value received from the app and calls `sendDataToClient()` function.
+
+📌 `BLYNK_WRITE(V4)`: This function is triggered when the value of virtual pin V4 is updated on the Blynk app. It updates the `remainingVolume1` variable with the new value received from the app.
+
+📌 `BLYNK_WRITE(V5)`: This function is triggered when the value of virtual pin V5 is updated on the Blynk app. It updates the `cost1` variable with the new value received from the app and calls `computeRemainingVolume()` and `sendDataToClient()` functions.
+
+📌 `computeRemainingVolume()`: This function calculates the remaining volume based on the `credits1` and `cost1` variables and updates the `remainingVolume1` variable. It also sends the updated `remainingVolume1` value to the Blynk app using the virtual pin V4.
+
+📌 `sendDataToClient()`: This function constructs a message using the `volume1`, `credits1`, and `cost1` variables and sends it to the specified destination device (Destination_ESP32_Slave_1) using LoRa communication. The message format is "Slave01/volume1&credits1#cost1".
+
+📌 `sendDataToBlynk()`: This function sends the current values of `credits1`, `volume1`, `remainingVolume1`, and `cost1` to the Blynk app using their respective virtual pins V1, V3, V4, and V5.
+
+📌 `ResetToDefaultData()`: This function sets the initial/default values for `cost1`, `volume1`, `credits1`, `cost2`, `volume2`, and `credits2`. It then calls `sendDataToBlynk()` to update the Blynk app with the default values.
+
+📌 `sendMessage(String Outgoing, byte Destination)`: This function is responsible for sending a message (Outgoing) to the specified destination device (Destination) using LoRa communication. It starts by beginning a LoRa packet, writes the destination address, sender address, payload length, and payload data, and then ends the packet and sends it.
+
+📌 `onReceive(int packetSize)`: This function is called when a LoRa packet is received. It reads the packet header bytes to determine the recipient, sender, and length of the incoming message. Then it reads the payload data and stores it in the `Incoming` variable. It checks the length of the incoming message and compares it to the received length to ensure data integrity. If the message is for the current device, it processes the message accordingly. If the message starts with "Hi", it prepares a response message based on the sender's address. If the message is a data update, it parses the values for `volume1`, `credits1`, and `cost1` and updates the variables accordingly. Finally, it calls `sendDataToBlynk()` to update the Blynk app with the received values.
+
+📌 `setup()`: This function is called once during the initialization of the ESP32. It initializes the serial communication, sets up LoRa pins, and initializes the LoRa module. It also initializes the Blynk connection with the specified authentication token and Wi-Fi credentials. Additionally, it sets up a timer to periodically call `sendDataToBlynk()` every 1 second.
+
+📌 `loop()`: This function is called repeatedly in a loop. It checks for incoming LoRa packets using `LoRa.parsePacket()`, processes the received packets using `onReceive()`, runs the Blynk library using `Blynk.run()`, and also runs the Blynk timer using `timer.run()`. This function ensures continuous communication with LoRa and Blynk services.
